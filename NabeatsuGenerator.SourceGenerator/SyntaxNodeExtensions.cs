30fdcd49a6426850b9027eac3c5e4cf008e5f149
aetos <aetos382@users.noreply.github.com>
2022-08-05 18:40:41 +0900

    属性を組み立てるコードがあまりにおぞましいのでヘルパー化する

---
+++ NabeatsuGenerator.SourceGenerator/SyntaxNodeExtensions.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace NabeatsuGenerator.SourceGenerator;
+
+internal static class SyntaxNodeExtensions
+{
+    public static TypeDeclarationSyntax WithAttribute(
+        this TypeDeclarationSyntax syntax,
+        Type attributeType,
+        IReadOnlyList<(string? ParameterName, object? Value)>? constructorArguments = null,
+        IReadOnlyList<(string MemberName, object? Value)>? namedArguments = null,
+        CancellationToken cancellationToken = default)
+    {
+        if (attributeType is null)
+        {
+            throw new ArgumentNullException(nameof(attributeType));
+        }
+
+        return WithAttribute(
+            syntax,
+            $"global::{attributeType.FullName}",
+            constructorArguments,
+            namedArguments,
+            cancellationToken);
+    }
+
+    public static TypeDeclarationSyntax WithAttribute(
+        this TypeDeclarationSyntax syntax,
+        INamedTypeSymbol attributeTypeSymbol,
+        IReadOnlyList<(string? ParameterName, object? Value)>? constructorArguments = null,
+        IReadOnlyList<(string MemberName, object? Value)>? namedArguments = null,
+        CancellationToken cancellationToken = default)
+    {
+        if (attributeTypeSymbol is null)
+        {
+            throw new ArgumentNullException(nameof(attributeTypeSymbol));
+        }
+
+        var name = attributeTypeSymbol.ToDisplayString(
+            SymbolDisplayFormat.FullyQualifiedFormat);
+
+        return WithAttribute(
+            syntax,
+            name,
+            constructorArguments,
+            namedArguments,
+            cancellationToken);
+    }
+
+    public static TypeDeclarationSyntax WithAttribute(
+        this TypeDeclarationSyntax syntax,
+        string attributeTypeName,
+        IReadOnlyList<(string? ParameterName, object? Value)>? constructorArguments = null,
+        IReadOnlyList<(string MemberName, object? Value)>? namedArguments = null,
+        CancellationToken cancellationToken = default)
+    {
+        if (syntax is null)
+        {
+            throw new ArgumentNullException(nameof(syntax));
+        }
+
+        if (attributeTypeName is null)
+        {
+            throw new ArgumentNullException(nameof(attributeTypeName));
+        }
+
+        var list = CreateAttributeLists(
+            attributeTypeName,
+            constructorArguments,
+            namedArguments,
+            cancellationToken);
+
+        syntax = syntax.WithAttributeLists(list);
+        return syntax;
+    }
+
+    private static SyntaxList<AttributeListSyntax> CreateAttributeLists(
+        string attributeTypeName,
+        IReadOnlyList<(string? ParameterName, object? Value)>? constructorArguments,
+        IReadOnlyList<(string MemberName, object? Value)>? namedArguments,
+        CancellationToken cancellationToken)
+    {
+        var totalCount =
+            constructorArguments?.Count ?? 0 +
+            namedArguments?.Count ?? 0;
+
+        var attributeArguments = new List<AttributeArgumentSyntax>(totalCount);
+
+        if (constructorArguments is not null)
+        {
+            foreach (var constructorArgument in constructorArguments)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var expression = ValueToExpression(constructorArgument.Value);
+
+                var argument = constructorArgument.ParameterName switch {
+                    null => SyntaxFactory.AttributeArgument(expression),
+                    var name => SyntaxFactory.AttributeArgument(
+                        default,
+                        SyntaxFactory.NameColon(name),
+                        expression)
+                };
+
+                attributeArguments.Add(argument);
+            }
+        }
+
+        if (namedArguments is not null)
+        {
+            foreach (var namedArgument in namedArguments)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var expression = ValueToExpression(namedArgument.Value);
+
+                var argument = namedArgument.MemberName switch {
+                    null => SyntaxFactory.AttributeArgument(expression),
+                    var name => SyntaxFactory.AttributeArgument(
+                        SyntaxFactory.NameEquals(name),
+                        default,
+                        expression)
+                };
+
+                attributeArguments.Add(argument);
+            }
+        }
+
+        var list = SyntaxFactory.List(
+            new[] {
+                SyntaxFactory.AttributeList(
+                    SyntaxFactory.SeparatedList(
+                        new[] {
+                            SyntaxFactory.Attribute(
+                                SyntaxFactory.ParseName(attributeTypeName),
+                                SyntaxFactory.AttributeArgumentList(
+                                    SyntaxFactory.SeparatedList(
+                                        attributeArguments)))
+                        }))
+            });
+
+        return list;
+    }
+
+    private static ExpressionSyntax ValueToExpression(
+        object? value)
+    {
+        ExpressionSyntax expression;
+
+        if (value is Type t)
+        {
+            expression = SyntaxFactory.TypeOfExpression(
+                SyntaxFactory.ParseTypeName(t.FullName));
+        }
+        else if (value is Enum e)
+        {
+            var enumType = e.GetType();
+
+            expression = SyntaxFactory.MemberAccessExpression(
+                SyntaxKind.SimpleMemberAccessExpression,
+                SyntaxFactory.IdentifierName(enumType.FullName),
+                SyntaxFactory.IdentifierName(Enum.GetName(enumType, e)));
+        }
+        else
+        {
+            var (kind, token) = value switch {
+                null => (SyntaxKind.NullLiteralExpression, SyntaxFactory.Token(SyntaxKind.NullKeyword)),
+                byte v => (SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(v)),
+                sbyte v => (SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(v)),
+                short v => (SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(v)),
+                ushort v => (SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(v)),
+                int v => (SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(v)),
+                uint v => (SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(v)),
+                long v => (SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(v)),
+                ulong v => (SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(v)),
+                string v => (SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal(v)),
+                char v => (SyntaxKind.CharacterLiteralExpression, SyntaxFactory.Literal(v)),
+                bool b => b switch {
+                    true => (SyntaxKind.TrueLiteralExpression, SyntaxFactory.Token(SyntaxKind.TrueKeyword)),
+                    false => (SyntaxKind.FalseLiteralExpression, SyntaxFactory.Token(SyntaxKind.FalseKeyword))
+                },
+                _ => throw new NotSupportedException()
+            };
+
+            expression = SyntaxFactory.LiteralExpression(kind, token);
+        }
+
+        return expression;
+    }
+}
