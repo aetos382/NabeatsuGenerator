c96fcc35f35a9e759a399a15bb32522d3c616d51
aetos <aetos382@users.noreply.github.com>
2022-08-05 18:44:56 +0900

    もう使ってない

--- NabeatsuGenerator.SourceGenerator/GenerationInfo.cs
+++ NabeatsuGenerator.SourceGenerator/GenerationInfo.cs
@@ -7,35 +7,4 @@ internal sealed record GenerationInfo(
     MethodDeclarationSyntax MethodNode,
     IMethodSymbol MethodSymbol,
     int Start,
-    int End)
-{
-    public bool Equals(
-        GenerationInfo? other)
-    {
-        if (other is null)
-        {
-            return false;
-        }
-
-        if (object.ReferenceEquals(this, other))
-        {
-            return true;
-        }
-
-        return
-            this.MethodNode.IsEquivalentTo(other.MethodNode) &&
-            SymbolEqualityComparer.Default.Equals(this.MethodSymbol, other.MethodSymbol) &&
-            this.Start == other.Start &&
-            this.End == other.End;
-    }
-
-    public override int GetHashCode()
-    {
-        return
-            HashCode.Combine(
-                this.MethodNode,
-                SymbolEqualityComparer.Default.GetHashCode(this.MethodSymbol),
-                this.Start,
-                this.End);
-    }
-}
+    int End);
